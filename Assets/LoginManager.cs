f519aa2868f5230553fa7f91c001a71079df4caf
dansilber08 <48389991@est.ort.edu.ar>
2024-05-27 09:03:27 -0300

    LoginManager script check password

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour
+
 {
+    string rightPassword = "dan123";
+    public InputField inputfield;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,21 @@ public class LoginManager : MonoBehaviour
     void Update()
     {
         
+    }
+    void checkLogIn(string password, string input)
+    {
+        if (input == password)
+        {
+            Debug.Log("Acceso concedido");
+        } 
+        else
+        {
+            Debug.Log("Acceso denegado");
+        }
+    
+    }
+    public void ButtonClick()
+    {
+        checkLogIn(rightPassword, inputfield.text);
     }
 }
